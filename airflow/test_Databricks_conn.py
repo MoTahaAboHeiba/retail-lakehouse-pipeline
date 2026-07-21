4ea16f304076eeeaeb0f2311ef018fda5bfbd1ec
Mohamed Taha Abo Hieba <mt1641936@gmail.com>
2026-07-21 18:19:20 +0300

    Update test_Databricks_conn.py

--- airflow/test_Databricks_conn.py
+++ airflow/test_Databricks_conn.py
@@ -1,7 +1,7 @@
 '''
 this script is a simple example of how to use the Databricks SDK to trigger a Databricks job from Python.
 It uses the WorkspaceClient class to connect to the Databricks workspace and run a job using its job ID. 
-Make sure to set the environment variables HOST, TOKEN, and DATABRICKS_JOB_ID before running this script. 
+Make sure to set the environment variables DATABRICKS_HOST, DATABRICKS_TOKEN, and DATABRICKS_JOB_ID before running this script. 
 You can set these variables in your operating system or in a .env file
 '''
 import os
@@ -10,8 +10,8 @@ from databricks.sdk import WorkspaceClient
 
 def run_databricks_job():
     ws = WorkspaceClient(
-        host=os.getenv("HOST"),
-        token=os.getenv("TOKEN")
+        host=os.getenv("DATABRICKS_HOST"),
+        token=os.getenv("DATABRICKS_TOKEN")
     )
 
     job_trigger = ws.jobs.run_now(job_id=os.getenv("DATABRICKS_JOB_ID"))
