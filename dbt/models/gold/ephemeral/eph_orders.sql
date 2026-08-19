7508b6109366e75c8cef61e557df4c0dac93ba80
Mohamed Taha Abo Heiba <mt1641936@gmail.com>
2026-08-19 18:50:49 +0300

    Remove unnecessary timestamp fields from eph_orders
    Removed created and updated timestamps from the select statement.

--- dbt/models/gold/ephemeral/eph_orders.sql
+++ dbt/models/gold/ephemeral/eph_orders.sql
@@ -3,7 +3,5 @@ SELECT
     payment_method,
     order_status,
     order_timestamp,
-    created_timestamp as order_created_timestamp,
-    updated_timestamp as order_updated_timestamp,
     is_active as order_is_active
 FROM {{ ref('orders_tech') }}
