5b834e7ec4c78abf394720c877695bd67646d9be
Mohamed Taha Abo Heiba <mt1641936@gmail.com>
2026-08-19 03:27:53 +0300

    Add files via upload

---
+++ dbt/models/gold/dimension/dim_products_current.sql
@@ -0,0 +1,12 @@
+SELECT
+    dbt_scd_id,
+    product_id,
+    product_name,
+    category,
+    brand,
+    price,
+    product_created_timestamp,
+    product_updated_timestamp,
+    product_is_active
+FROM {{ ref('dim_products') }}
+WHERE dbt_valid_to = to_date('9999-12-31')
