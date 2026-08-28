7036ba2910f0fdbda060a5a9eeb1bc0b036b7876
Mohamed Taha Abo Heiba <mt1641936@gmail.com>
2026-08-28 09:55:12 +0300

    Include customer_updated_timestamp in eph_customers
    Added customer_updated_timestamp to the selection.

--- dbt/models/gold/ephemeral/eph_customers.sql
+++ dbt/models/gold/ephemeral/eph_customers.sql
@@ -8,6 +8,7 @@ SELECT
     customer_city,
     customer_province,
     customer_country,
-    customer_is_active
+    customer_is_active,
+    customer_updated_timestamp
 FROM 
     {{ ref('obt_business') }}
