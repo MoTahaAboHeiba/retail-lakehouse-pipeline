f41d5af09edfa474b648fac4016d0f6aea6d7a0d
Mohamed Taha Abo Heiba <mt1641936@gmail.com>
2026-08-28 09:55:55 +0300

    Add updated_timestamp to eph_orders SQL query

--- dbt/models/gold/ephemeral/eph_orders.sql
+++ dbt/models/gold/ephemeral/eph_orders.sql
@@ -3,5 +3,6 @@ SELECT
     payment_method,
     order_status,
     order_timestamp,
-    is_active as order_is_active
+    is_active as order_is_active,
+    updated_timestamp as order_updated_timestamp
 FROM {{ ref('orders_tech') }}
